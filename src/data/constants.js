e5d07ba33db543f85a2652d761d8e6260e0bd79c
Rudr Pratap Singh <rudrpsingh@hotmail.com>
2024-06-29 17:33:39 +0530

    init

---
+++ src/data/constants.js
@@ -0,0 +1,328 @@
+export const Bio = {
+  name: "Rudr Pratap Singh",
+  roles: [
+    "Software Developer",
+    "ML Engineer",
+    "Android Developer",
+    "Programmer",
+  ],
+  description:
+    "As a member of three student clubs at Netaji Subhas University of Technology, I apply my AI skills to various projects and challenges, such as developing a deep learning model for image classification, creating a chatbot for campus queries, and analyzing data from astronomical observations. I also collaborate with other students and mentors to learn new technologies, share ideas, and solve problems.",
+  github: "https://github.com/rzzdr",
+  resume:
+    "https://drive.google.com/file/d/1XsbJsY9w6QyrFNn7iShEG03k4qkEmjnp/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rudr-pratap-singh",
+  twitter: "https://x.com/rudrpsingh",
+  insta: "https://www.instagram.com/rudrninikarra/",
+};
+
+export const skills = [
+  {
+    title: "Machine Learning",
+    skills: [
+      {
+        name: "Jupyter",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jupyter/jupyter-original.svg"
+      },
+      {
+        name: "Scikit Learn",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/scikitlearn/scikitlearn-original.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg",
+      },
+      {
+        name: "MatplotLib",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/matplotlib/matplotlib-original.svg",
+      },
+      {
+        name: "Seaborn",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/seaborn-1.svg",
+      },
+      {
+        name: "Numpy",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/numpy/numpy-original.svg",
+      },
+      {
+        name: "Pandas",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pandas/pandas-line.svg",
+      },
+    ],
+  },
+  {
+    title: "Deep Learning",
+    skills: [
+      {
+        name: "TensorFlow",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tensorflow/tensorflow-original.svg"
+      },
+      {
+        name: "PyTorch",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pytorch/pytorch-original.svg"
+      },
+      {
+        name: "HuggingFace",
+        image:
+          "https://huggingface.co/datasets/huggingface/brand-assets/resolve/main/hf-logo.svg"
+      },
+      {
+        name: "Keras",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/keras/keras-original.svg"
+      },
+      {
+        name: "Kaggle",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kaggle/kaggle-original.svg"
+      },
+      {
+        name: "Azure ML",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azure/azure-original.svg"
+      },
+      {
+        name: "AWS",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
+      },
+    ]
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "XML",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/xml/xml-original.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+      {
+        name: "Jetpack Compose",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/jetpackcompose/jetpackcompose-original.svg"
+      }
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Linux",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg",
+      },
+      {
+        name: "Arch Linux",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/archlinux/archlinux-original.svg",
+      },
+      {
+        name: "Git",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-original.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "PyCharm",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/pycharm/pycharm-original.svg"
+      },
+      {
+        name: "Figma",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/figma/figma-original.svg",
+      },
+      {
+        name: "Bash",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/bash/bash-plain.svg"
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/gdsc.jpeg?alt=media&token=c162329c-efaa-4be8-a173-8d3f4c48ea70",
+    role: "Machine Learning Department Member",
+    company: "Google DSC NSUT",
+    date: "Sep 2023 - Present",
+    desc: "As member of Machine Learning Department, I used Google's Gemini/Bard as well as other LLMs (Falcon, LLaMA, LaMDA, PaLM) to build scalable applications. Performed core NLP based tasks such as NER, similarity search etc. Also used prompt engineering as well as finetuning for various tasks. Used Azure and AWS for deployment, worked with a Github CI/CD pipeline for seamless workflow",
+    skills: [
+      "Leadership",
+      "Machine Learning",
+      "AI",
+      "Deep Learning",
+      "Computer Vision",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://img.collegepravesh.com/2015/10/NSUT-Delhi-Logo.png",
+    school: "Netaji Subhas University of Technology,   New Delhi",
+    date: "Aug 2023 - Jul 2027",
+    grade: "6.91 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Electronics and Communication Engineering with specialization in Artificial Intelligence and Machine Learning at Netaji Subhas University of Technology, Dwarka, New Delhi. I have completed 2 semesters and have a CGPA of 6.91. I have taken courses in Data Structures, Algorithms, Python Programming, Network Analysis and Synthesis, Electronic Devices and Circuits and Fundamentals of Electrical Engineering, among others. I am also a member of the Google Developers Student Club (GDSC) at NSUT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Electronics and Communication Engineering with specialization in Artificial Intelligence and Machine Learning",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C510BAQHlQcY_FIqWlg/company-logo_200_200/0/1630564183546/delhi_public_school_sector_19_faridabad_logo?e=2147483647&v=beta&t=jex-E53VJWmsEwxl4TQ8ggFIKJX0g_m1NfTQZUupLgA",
+    school: "Delhi Public School, Faridabad",
+    date: "Apr 2021 - Apr 2023",
+    grade: "85.0%",
+    desc: "I completed my class 12 high school education at Delhi Public School, Faridabad, where I studied Non-Medical Science with Typography.",
+    degree: "CBSE(XII), Non-Medical Science(PCM) with Typography",
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C510BAQHlQcY_FIqWlg/company-logo_200_200/0/1630564183546/delhi_public_school_sector_19_faridabad_logo?e=2147483647&v=beta&t=jex-E53VJWmsEwxl4TQ8ggFIKJX0g_m1NfTQZUupLgA",
+    school: "Delhi Public School, Faridabad",
+    date: "Apr 2020 - Apr 2021",
+    grade: "82.3%",
+    desc: "I completed my class 10 high school education at Delhi Public School, Faridabad.",
+    degree: "CBSE(X)",
+  },
+];
+
+export const projects = [
+  {
+    id: 3,
+    title: "Early signs of Cirrhosis prediction",
+    date: "Nov 2023",
+    description:
+      "Used a multi-class approach to predict the the outcomes of patients with cirrhosis.",
+    image:
+      "https://www.kaggle.com/competitions/60893/images/header",
+    tags: ["Pandas", "Numpy", "Data Science", "AI", "Kaggle"],
+    category: "machine learning",
+    github: "https://github.com/rzzdr/Early-signs-of-Cirrhosis-prediction",
+    webapp: "https://github.com/rzzdr/Early-signs-of-Cirrhosis-prediction",
+    member: [
+      {
+        name: "Rudr Pratap Singh",
+        img: "https://avatars.githubusercontent.com/u/145427916?v=4",
+        linkedin: "https://www.linkedin.com/in/rudr-pratap-singh/",
+        github: "https://github.com/rzzdr/",
+      }
+    ]
+  },
+  {
+    id: 0,
+    title: "Deepfake Detection with Attention Mechanism",
+    date: "Jan 2024",
+    description:
+      "Generative adversarial networks (GANs) can be used to generate a photo-realistic image from a low-dimension random noise. Such a synthesized (fake) image with inappropriate content can be used on social media networks, which can cause severe problems. With the aim to successfully detect fake images, an effective and efficient image forgery detector is necessary. So, I present a DeepFake Detection Model.",
+    image:
+      "https://github.com/rzzdr/Deepfake-Detection/raw/main/Deepfake.png",
+    tags: ["LSTM", "EfficientNet-B7", "Computer Vision", "AI"],
+    category: "machine learning",
+    github: "https://github.com/rzzdr/Deepfake-Detection/",
+    webapp: "https://github.com/rzzdr/Deepfake-Detection/",
+    member: [
+      {
+        name: "Rudr Pratap Singh",
+        img: "https://avatars.githubusercontent.com/u/145427916?v=4",
+        linkedin: "https://www.linkedin.com/in/rudr-pratap-singh/",
+        github: "https://github.com/rzzdr/",
+      }
+    ]
+  },
+  {
+    id: 1,
+    title: "Indian Sign Language Translator",
+    date: "Feb 2024",
+    description:
+      " The processed input(Processed using a YOLO-v3 pretrained network for hand's frame detection with combination of HSV (Hue, Saturation, Value) and YCbCr (Luminance, Chrominance) based filters to segment out skin and remove background noise) is passed through a SqueezeNet model trained (via Transfer Learning) on a synthesized and cleaned Indian Sign Language dataset consisting of 10 classes, and ~2300+ images per class.",
+    image:
+      "https://user-images.githubusercontent.com/39513876/118981340-dcc48900-b997-11eb-98d8-686dd402f544.png",
+    tags: ["YOLO-v3", "SqueezeNet", "Computer Vision", "AI", "OpenCV"],
+    category: "machine learning",
+    github: "https://github.com/rzzdr/Indian-Sign-Language-Translator/",
+    webapp: "https://github.com/rzzdr/Indian-Sign-Language-Translator/",
+    member: [
+      {
+        name: "Rudr Pratap Singh",
+        img: "https://avatars.githubusercontent.com/u/145427916?v=4",
+        linkedin: "https://www.linkedin.com/in/rudr-pratap-singh/",
+        github: "https://github.com/rzzdr/",
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "Financial Nexus",
+    date: "Apr 2024",
+    description:
+      "Financial Nexus is your one-stop shop for managing money! Track expenses, income, investments, and debts - all in one place. Plus, our built-in Large Language Model (LLM) provides safe, personalized investment advice based on your financial data and goals. No judgment, just smart strategies to help you reach your financial dreams!",
+    image:
+      "https://github.com/coderkhushal/financial-nexus-Frontend/blob/main/public/financial-nexus-logo.jpg?raw=true",
+    tags: ["Python", "Gemini", "LLM", "NLP", "AI"],
+    category: "machine learning",
+    github: "https://github.com/atishayj2202/Financial-Nexus-Backend/blob/main/src/services/ai.py",
+    webapp: "https://financial-nexus.vercel.app",
+    member: [
+      {
+        name: "Rudr Pratap Singh",
+        img: "https://avatars.githubusercontent.com/u/145427916?v=4",
+        linkedin: "https://www.linkedin.com/in/rudr-pratap-singh/",
+        github: "https://github.com/rzzdr/",
+      },
+      {
+        name: "Atishay Jain",
+        img: "https://avatars.githubusercontent.com/u/53685605?v=4",
+        linkedin: "https://www.linkedin.com/in/atishayj2202/",
+        github: "https://github.com/atishayj2202",
+      },
+      {
+        name: "Khushal Bhasin",
+        img: "https://avatars.githubusercontent.com/u/137866759?v=4",
+        linkedin: "https://www.linkedin.com/in/khushal-bhasin-78504a284/",
+        github: "https://github.com/coderkhushal"
+      },
+      {
+        name: "Navtesh Maken",
+        img: "https://media.licdn.com/dms/image/D4D03AQEgnqomQMZFpA/profile-displayphoto-shrink_400_400/0/1690828602078?e=1724889600&v=beta&t=AInYHcAmQe1fsYwtU8ORAw3DBDB54X3YGgeMYiMyoVM",
+        linkedin: "https://www.linkedin.com/in/navtesh-maken-95a412286/",
+        github: "https://github.com/navtesh21"
+      },
+      {
+        name: "Swayam Singh",
+        img: "https://media.licdn.com/dms/image/D4D03AQETtb6Jtap-6Q/profile-displayphoto-shrink_400_400/0/1693497499932?e=1724889600&v=beta&t=fB9c2SBkZkr1WvB9SHCwcW4U9YOb7rcyUkSui5d04FU",
+        linkedin: "https://www.linkedin.com/in/swayam-singh-1a434428b/"
+      }
+    ]
+  },
+];
